e971ac86b439a75177f7f31efbc6551dddc01f77
Abdul Afeel <abdulafeel2@gmail.com>
2023-01-20 15:12:51 +0530

    ngrok change

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,22 +1,21 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const instance =axios.create({
-    baseURL:'https://6b95-27-111-75-203.in.ngrok.io'
+const instance = axios.create({
+  baseURL: "https://c6e7-27-111-75-214.in.ngrok.io",
 });
 
 instance.interceptors.request.use(
-   async (config)=>{
-
-    const token = await AsyncStorage.getItem('token');
-    if(token){
-        config.headers.Authorization= `Bearer ${token}`;
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-        return config;
-    },
-    (err)=>{
-        return Promise.reject(err);
-    }
-)
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
 
 export default instance;
